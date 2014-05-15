b1a486f92438119e426f37a6747d181c98ccd787
Levi Ringleka <leviringleka@gmail.com>
2014-05-15 10:03:30 -0600

    Continuation of piece placement class and additions to input file document.
    Continuing on the way toward completion and better tests on the classes.

--- Projects/ConsoleBoardDisplay.java
+++ Projects/ConsoleBoardDisplay.java
@@ -4,9 +4,20 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ConsoleBoardDisplay {
+	
+	private static final Pattern pattern1 = Pattern.compile("([KQNRBP])([ld])([a-h])([1-8])" );
+	private static final Pattern pattern2 = Pattern.compile("([a-h])([1-8]) ([a-h])([1-8])");
+	private static final Pattern pattern3 = Pattern.compile("([a-h])([1-8]) ([a-h])([1-8])[*]");
+	private static final Pattern pattern4 = Pattern.compile("([a-h])([1-8]) ([a-h])([1-8]) ([a-h])([1-8]) ([a-h])([1-8])");
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		File f = new File(args[0]);
 		FileInputStream is = new FileInputStream(f);
@@ -18,6 +29,63 @@ public class ConsoleBoardDisplay {
 				board[i][j] = '-';
 			}
 		}
+		
+		Matcher m4;
+		Matcher m3;
+		Matcher m2;
+		Matcher m1;
+		String next;
+		
+		try {
+			next = br.readLine();
+			while (next != null) {
+				m1 = pattern1.matcher(next);
+				m2 = pattern2.matcher(next);
+				m3 = pattern3.matcher(next);
+				m4 = pattern4.matcher(next);
+				Map<Character, String> key = new HashMap<Character, String>();
+				key.put('K', " king ");
+				key.put('Q', " queen ");
+				key.put('N', " knight ");
+				key.put('R', " rook ");
+				key.put('B', " bishop ");
+				key.put('P', " pawn ");
+
+				if (m4.find()) {
+					/*if(board[(int)m4.group(1).toLowerCase().toCharArray()[0] - 97][(int)m4.group(2).toCharArray()[0] - 49] != '-' && board[(int)m4.group(5).toLowerCase().toCharArray()[0] - 97][(int)m4.group(6).toCharArray()[0] - 49] != '-'){
+						
+					}*/
+					
+				} else if (m3.find()) {
+					
+					
+				} else if (m2.find()) {
+					if(board[(int)m2.group(1).toLowerCase().toCharArray()[0] - 97][(int)m2.group(2).toCharArray()[0] - 49] != '-'){
+						char piece = board[(int)m2.group(1).toLowerCase().toCharArray()[0] - 97][(int)m2.group(2).toCharArray()[0] - 49];
+						board[(int)m2.group(1).toLowerCase().toCharArray()[0] - 97][(int)m2.group(2).toCharArray()[0] - 49] = '-';
+						board[(int)m2.group(3).toLowerCase().toCharArray()[0] - 97][(int)m2.group(4).toCharArray()[0] - 49] = piece;
+					}
+				} else if (m1.find()) {
+
+					
+					String piece = m1.group(1);
+					if(m1.group(2).equals("l")){
+						piece = piece.toLowerCase();
+					
+					}
+					board[(int)m1.group(3).toLowerCase().toCharArray()[0] - 97][(int)m1.group(4).toCharArray()[0] - 49] = piece.toCharArray()[0];
+					
+				}
+				else {
+					
+				}
+				next = br.readLine();
+				
+			}
+		} catch (IOException e) {
+
+		}
+		
 		for(int i = 0; i < 8; i++){
 			for(int j = 0; j < 8; j++){
 				System.out.print(board[i][j]);
